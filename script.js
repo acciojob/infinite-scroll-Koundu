2f92639343c5d10af8b2f8f579918c142f80b060
gk1706 <121346044+Koundu@users.noreply.github.com>
2023-09-20 15:19:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
+const scroll = document.getElementById('infi-list')
+let count = 0;
 
+
+scroll.addEventListener('click'()=>{
+	if(count<10){
+		count ==10;
+		for(let i=0;i<count;i++){
+			addItem(count);
+		}
+	}
+})
+
+scroll.addEventListener('scroll',()=>{
+	addItem(count);
+	addItem(count);
+})
+function addItem(count){
+	let para = document.createElement('p');
+	para.innerText = "Item "+count;
+	scroll.appendChild(para);
+	count++
+	console.log(count)
+}
